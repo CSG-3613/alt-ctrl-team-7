3ad58db292a75624ae6eb449d28c9a5c3ef682b4
Haiying Zeng <122418347+hertz30@users.noreply.github.com>
2024-04-16 13:16:24 -0500

    Added obstacle generation logic, fixed tunnel instantiation

---
+++ AltCtrl/Assets/Scripts/TunnelMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelMovement : MonoBehaviour
+{
+    public float velocity = 2f;
+    public float endTunnelZ = -40;
+    public GameObject SpawnPosition;
+    public static Transform startingPosition;
+    private float trackMotion;
+    public GameObject[] tunnelPrefabs;
+    public GameObject[] obstaclePrefabs;
+    public float chanceObstacle = 0.5f;
+
+    private void Start()
+    {
+        if(SpawnPosition != null && startingPosition == null)
+        {
+            startingPosition = SpawnPosition.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = gameObject.transform.position + (Vector3.back)*velocity*Time.deltaTime;
+        if (this.gameObject.transform.position.z<=endTunnelZ)
+        {
+            Instantiate(tunnelPrefabs[Random.Range(0, tunnelPrefabs.Length)], startingPosition.position, startingPosition.rotation);
+            if (Random.value < chanceObstacle)
+            {
+                Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)],
+                    startingPosition.position + new Vector3(-2.5f, 1.5f, 0) //center reference point
+                    + Random.Range(-5.0f, 5.0f) * Vector3.right + Random.Range(-5.0f, 5.0f) * Vector3.up, //repositioning logic
+                    startingPosition.rotation);
+            }
+            Destroy(gameObject);
+
+        }
+    }
+}
