c3192680f91ff9c36b9917495152e8efee7e2475
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2024-04-16 22:18:39 -0500

    added a form of rotation to player movement

--- AltCtrl/Assets/Scripts/PlayerInputController.cs
+++ AltCtrl/Assets/Scripts/PlayerInputController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerInputController : MonoBehaviour, PlayerControlls.IPlayerMovementActions
 {
     private PlayerControlls.PlayerMovementActions actions;
     //refrence to the action map we are implementing
-    private float _scrollSpeed;
+    private Rigidbody _rigidBody;
+    //refrence to the action map we are implementing
+
+
+    [SerializeField]
+    private float _verticalForce;
     //scroll speed of the mouse wheel
+    
+    private float targetH = 0f;
+    private float targetV = 0f;
+    private float tempAngleH;
+    private float tempAngleV;
+    private bool input;
+
+    private float maxHorizontalTurn = 45;
+    private float maxVerticalTurn = 45;
+    private float rollSpeed = 20;
+    private float pitchSpeed = 20;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get a refrence to our rigidbody
+        _rigidBody = GetComponent<Rigidbody>();
+
         //Get a reference to our action map
         actions = new PlayerControlls().PlayerMovement;
         //Enable our actions
@@ -22,26 +43,80 @@ public class PlayerInputController : MonoBehaviour, PlayerControlls.IPlayerMovem
 
     }
 
-// Update is called once per frame
-void Update()
+    void FixedUpdate()
     {
-        Debug.Log("Speed: " + (_scrollSpeed / Time.deltaTime));
+        float threshold = 0.0005f;
+
+        //Get angle between current and targetH
+        float angleDelta = targetH-tempAngleH;
+
+        //if we are outside a threshold
+        if (Mathf.Abs(angleDelta) > threshold)
+        {
+            //Use angle to determine direction to turn
+            float direction = 1 * Mathf.Sign(angleDelta);
+
+            //Turn towards angle at given speed
+            tempAngleH += Time.deltaTime * rollSpeed * direction;
+        }
+        //else, set angle to targetH
+        else
+        {
+            tempAngleH = targetH;
+        }
+
+        angleDelta = targetV - tempAngleV;
+
+        //if we are outside a threshold
+        if (Mathf.Abs(angleDelta) > threshold)
+        {
+            //Use angle to determine direction to turn
+            float direction = 1 * Mathf.Sign(angleDelta);
+
+            //Turn towards angle at given speed
+            tempAngleV += Time.deltaTime * pitchSpeed * direction;
+        }
+        //else, set angle to targetH
+        else
+        {
+            tempAngleV = targetV;
+        }
+
+        transform.rotation = Quaternion.Euler(tempAngleV, 0, tempAngleH);
+    }
+
+
+
+    public void OnRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _rigidBody.AddForce(new Vector2(_verticalForce, 0), ForceMode.Force);
+            input = true;
+            targetH -= maxHorizontalTurn;
+
+        }
+        else if (context.canceled)
+        {
+            //targetH += maxHorizontalTurn;
+            input = false;
+        }
+
     }
 
     public void OnLeft(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log("Left");
+            _rigidBody.AddForce(new Vector2(-_verticalForce, 0), ForceMode.Force);
+            input = true;
+            targetH += maxHorizontalTurn;
+
         }
-
-    }
-
-    public void OnRight(InputAction.CallbackContext context)
-    {
-        if (context.performed)
+        else if (context.canceled)
         {
-            Debug.Log("Right");
+            targetH -= maxHorizontalTurn;
+            input = false;
         }
     }
 
@@ -49,9 +124,14 @@ void Update()
     {
         if (context.performed)
         {
-            _scrollSpeed += (context.ReadValue<float>() / 120);
-            Debug.Log("scroll");
-            
+            _rigidBody.AddForce(new Vector2(0, _verticalForce), ForceMode.Force);
+            targetV -= maxVerticalTurn;
+            Debug.Log(targetV);
+        }
+        else if (context.canceled)
+        {
+            //targetV += maxVerticalTurn;
+            input = false;
         }
     }
 }
