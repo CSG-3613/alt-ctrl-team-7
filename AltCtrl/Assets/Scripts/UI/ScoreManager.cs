ec6d0ca316d21c19252b39cfc9e5fa4780357074
KeklordNappa <wlg9387@utulsa.edu>
2024-04-17 15:34:12 -0500

    TMP and scoremanager made

---
+++ AltCtrl/Assets/Scripts/UI/ScoreManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    //[SerializeField] public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
