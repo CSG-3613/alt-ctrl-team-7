22d898f24d4c72f53b0202814e52295df506ba52
Haiying Zeng <hzeng30@gmail.com>
2024-04-22 16:54:23 -0500

    Fixed obstacle generation issue

--- AltCtrl/Assets/Scripts/TunnelMovement.cs
+++ AltCtrl/Assets/Scripts/TunnelMovement.cs
@@ -34,13 +34,15 @@ public class TunnelMovement : MonoBehaviour
         gameObject.transform.position = gameObject.transform.position + (Vector3.back)*velocity*Time.deltaTime;
         if (this.gameObject.transform.position.z<=endTunnelZ)
         {
+            if(gameObject.layer == 3){
             Instantiate(tunnelPrefabs[Random.Range(0, tunnelPrefabs.Length)], startingPosition.position, startingPosition.rotation);
-            if (Random.value < chanceObstacle)
-            {
+                if (Random.value < chanceObstacle)
+                {
                 Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)],
                     startingPosition.position + new Vector3(-2.5f, 1.5f, 0) //center reference point
                     + Random.Range(-5.0f, 5.0f) * Vector3.right + Random.Range(-5.0f, 5.0f) * Vector3.up, //repositioning logic
                     startingPosition.rotation);
+                }
             }
             Destroy(gameObject);
 
