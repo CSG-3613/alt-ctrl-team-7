3857a56d8e4776aa55b21daaa21faf7a7cecc4db
KeklordNappa <wlg9387@utulsa.edu>
2024-04-22 15:24:59 -0500

    TODO: die

--- AltCtrl/Assets/Scripts/GameManager.cs
+++ AltCtrl/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -43,5 +44,10 @@ public class GameManager : MonoBehaviour
     public float getCurrSpeed()
     {
         return currSpeed;
+    }
+
+    public void resetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
