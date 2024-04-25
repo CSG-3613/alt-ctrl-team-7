ff284336518f2d61543dfc71ff3e0be705b74579
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2024-04-25 01:41:31 -0500

    Added Multi Planet Object Spawning
    The tunnel and obstacles are each stored in an array, one for each per planet. Then those arrays are fed into a 2D array that manages the spawning of both. When the current planet index gets incremented, it moves to the next set of tiles.

--- AltCtrl/Assets/Scripts/TunnelManager.cs
+++ AltCtrl/Assets/Scripts/TunnelManager.cs
@@ -8,14 +8,28 @@ public class TunnelManager : MonoBehaviour
 
     public static TunnelManager TMinstance { get; private set; }
 
-    public static GameManager gm;
+    private static GameManager gm;
 
-    public float velocity;
-    public GameObject LastTunnel;   
-    public GameObject[] tunnelPrefabs;
-    public GameObject[] obstaclePrefabs;
-    public float chanceObstacle = 0.5f;
+    [SerializeField] private float velocity;
+    [SerializeField] private GameObject LastTunnel;
 
+    [Header("Planet Tunnel/Objects")]
+    //index of the current planet, used to swap what set of arrays are being used for obsticle and tunnel generation
+    [SerializeField] private int _currentPlanetIndex;
+    //arrays for all the different planets
+    [SerializeField] private GameObject[] _Planet1TunnelPrefabs;
+    [SerializeField] private GameObject[] _Planet1ObstaclePrefabs;
+    [SerializeField] private GameObject[] _Planet2TunnelPrefabs;
+    [SerializeField] private GameObject[] _Planet2ObstaclePrefabs;
+    //chance to spawn an obsticle
+    [SerializeField] private float chanceObstacle = 0.5f;
+
+    //2D array initialization needs a static int value, This cannot be seen in the inspector
+    private static int _numberOfPlanets = 2;
+
+    //2D Arrays Cannot be seen in inspector! Must be assigned in code!
+    private GameObject[][] tunnelPrefabs = new GameObject[_numberOfPlanets][];
+    private GameObject[][] ObstaclePrefabs = new GameObject[_numberOfPlanets][];
 
     private void Awake()
     {
@@ -25,32 +39,49 @@ public class TunnelManager : MonoBehaviour
         //Check that Last tunnel has been assigned
         if (LastTunnel == null) { Debug.LogError("Assign Last tunnel in inspector!!!"); }
 
+        //Assigning the values of the 2d Array
+        Initialize2DArrays();
+
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         //get reference to GameManager
         gm = GameManager.instance;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         velocity = gm.getCurrSpeed();
     }
 
-    public void SpawnTunnel()
+    public void SpawnTunnel(GameObject _TunnelToDelete)
     {
-        GameObject nextTunnel = Instantiate(tunnelPrefabs[Random.Range(0, tunnelPrefabs.Length)], LastTunnel.transform.position + (5 - Time.deltaTime * velocity) * Vector3.forward, LastTunnel.transform.rotation);
+        GameObject nextTunnel = Instantiate(
+            tunnelPrefabs[_currentPlanetIndex][Random.Range(0, tunnelPrefabs[_currentPlanetIndex].Length)], //Gameobject to instantiate
+            LastTunnel.transform.position + (5 - Time.deltaTime * velocity) * Vector3.forward, //TunnelPosition
+            LastTunnel.transform.rotation); //Tunnel Rotation
 
         if (Random.value < chanceObstacle)
         {
-            Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)],
+            Instantiate(
+                ObstaclePrefabs[_currentPlanetIndex][Random.Range(0, ObstaclePrefabs[_currentPlanetIndex].Length)], //Gameobject to instantiate
                 LastTunnel.transform.position + new Vector3(-2.5f, -4f, 0) //center reference point
                 + Random.Range(-5.0f, 5.0f) * Vector3.right + Random.Range(-5.0f, 5.0f) * Vector3.up, //repositioning logic
                 LastTunnel.transform.rotation);
         }
         LastTunnel = nextTunnel;
+        //Destroy the tunnel that called the spawner
+        Destroy(_TunnelToDelete);
+    }
+
+    private void Initialize2DArrays()
+    {
+        //Assigning the values of the 2d Array
+        //Must have one for each number of planets!!
+        tunnelPrefabs[0] = _Planet1TunnelPrefabs;
+        ObstaclePrefabs[0] = _Planet1ObstaclePrefabs;
+        tunnelPrefabs[1] = _Planet2TunnelPrefabs;
+        ObstaclePrefabs[1] = _Planet2ObstaclePrefabs;
     }
 }
